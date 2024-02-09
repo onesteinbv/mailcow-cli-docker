820eb2dd376ef4ea767258570e8250829a5dd85a
tarteo <d.sluijk@onestein.nl>
2024-02-09 14:08:47 +0100

    Add echos

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 set -Eeuo pipefail
 
 mkdir /root/.config
+echo "Dockerize...";
 dockerize -template /tmp/templates/python-mailcow.conf.tmpl:/root/.config/python-mailcow.conf
+echo "Done";
 
 exec "$@"
