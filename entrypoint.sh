1671f169428142310b9bdf75e1618cebe9ab875f
tarteo <d.sluijk@onestein.nl>
2024-02-16 10:13:06 +0100

    Remove unnecessary echos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-echo "Dockerize...";
 mkdir /root/.config
 dockerize -template /tmp/templates/python-mailcow.conf.tmpl:/root/.config/python-mailcow.conf
 if [ ! -e /root/.config/python-mailcow.conf ]; then
   echo "Dockerize failed"
   exit 1
 fi
-echo "Done";
 
 ENV_ARGS=$(python /tmp/scripts/env_to_args.py)
 
